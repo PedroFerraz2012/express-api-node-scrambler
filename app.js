16410c57bc3df2d64e47dad004d037b3e971b0ac
Pedro Ferraz <pedroferraz2012@gmail.com>
2020-01-23 21:26:23 +1100

    starting handling errors with Morgan

--- app.js
+++ app.js
@@ -14,10 +14,27 @@ const orderRoutes = require('./api/routes/orders');
 //     });
 // });
 
+//using morgan
 app.use(morgan('dev'));
 
 //filter for produts Routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// starting handling error and request
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+// handling error
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
