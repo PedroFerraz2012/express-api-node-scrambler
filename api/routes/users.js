10328ed24c15e58e93747cc3b7e35dbfa8d52ea5
Pedro Ferraz <pedroferraz2012@gmail.com>
2020-01-27 00:23:22 +1100

    installing token, needs fixing

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 //import mongoose
 const mongoose = require('mongoose');
+//importing encryption
 const bcrypt = require('bcrypt');
+//importing sjonwebtoken
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -56,26 +59,37 @@ router.post('/login', (req, res, next) => {
     User.find({ email: req.body.email })
         .exec()
         .then(user => {
-            if(user.length < 1){
+            if (user.length < 1) {
                 return res.status(401).json({
                     message: 'Auth failed'
                 });
             }
             //comparing the hash has the same algorithm
-            bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
-                if(err) { return res.status(401).json({
-                    message: 'Auth failed'
-                });
-
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
                 }
-                if (result){ 
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        process.env.JWT_KEY,//insert JWT key in nodemon env
+                        { // inserting options
+                            expiresIn: "1h" //timeout
+                        }
+                    );
                     return res.status(200).json({
-                        message: 'Auth successful'
+                        message: 'Auth successful',
+                        token: token
                     });
                 }
                 res.status(401).json({
                     message: 'Auth failed'
-            });
+                });
             });
         })
         .catch(err => {
